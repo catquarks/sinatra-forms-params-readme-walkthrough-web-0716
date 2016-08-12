f1bef2f584aa782681a6567e74b1e4c7ce54dd8b
Tami <catquarks@gmail.com>
2016-08-11 20:24:23 -0400

    passed tests edited test to have an exclamation point at the end!

--- app.rb
+++ app.rb
@@ -2,8 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/food_form' do
-    erb :food_form
+
+	get '/food' do 
+		erb :food_form
+	end
+
+  post '/food' do
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}!"
   end
 
   # Add your post route and action below
